6c34cc3c1b0f909ba38ca516c369728ea3a301d2
Ankita Nag <ankeetaa3@gmail.com>
2025-03-31 21:39:10 +0530

    function basics

---
+++ JS_func/func.js
@@ -0,0 +1,5 @@
+function greeting(name="Jia"){ //if name not provided as an argument while calling the function then Jia will be the name else the given name will be printed
+    console.log(`Hello! ${name}`);
+    
+}
+greeting("Bar")
