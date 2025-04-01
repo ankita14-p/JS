773e9fb76ebf55e77491d3edca59186abdf9a6f0
Ankita Nag <ankeetaa3@gmail.com>
2025-04-01 23:53:23 +0530

    changes made

--- JS_func/func.js
+++ JS_func/func.js
@@ -1,5 +1,5 @@
-function greeting(name="Jia"){ //if name not provided as an argument while calling the function then Jia will be the name else the given name will be printed
+function greeting(name="User"){ //if name not provided as an argument while calling the function then Jia will be the name else the given name will be printed
     console.log(`Hello! ${name}`);
     
 }
-greeting("Bar")
+greeting("Ank")
